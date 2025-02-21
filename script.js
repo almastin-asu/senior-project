7ded5722a6df6797d1ad42db7fc3f36952448972
almastin-asu <116909483+almastin-asu@users.noreply.github.com>
2025-02-20 22:29:14 -0500

    style

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 "use strict"
 
+//INITIATIVE
+
+
+//ENCOUNTER GENERATOR
+
+const roll1 = document.getElementById("roll1");
+const roll2 = document.getElementById("roll2");
+const roll3 = document.getElementById("roll3");
+const roll4 = document.getElementById("roll4");
+
+
+
+
+
+//Event Handlers
+
+//INITIATIVE
+
+
+//ENCOUNTER GENERATOR
+
